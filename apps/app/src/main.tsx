f2ca804b8a2aa8c2689d813ceea92390a32c6b53
minhungle-me <haless132@gmail.com>
2022-04-30 14:55:42 +0700

    add somthing

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,13 +1,38 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './app/app';
+
+export let theme = createTheme({});
+theme = responsiveFontSizes(theme);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ToastContainer
+          newestOnTop
+          closeOnClick
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          rtl={false}
+        />
+
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
